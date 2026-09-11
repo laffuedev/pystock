1e0ee13b31f3ad01f60e9dbf34942d7549a2868f
JCL_developer <juanc.laffue@gmail.com>
2026-09-11 13:01:28 -0300

    feat: agregar funcion agregar_producto

--- inventario.py
+++ inventario.py
@@ -1,1 +1,12 @@
 # inicializar proyecto pystock
+
+productos = []
+
+def agregar_producto(nombre, cantidad, precio):
+    if cantidad <= 0:
+        raise ValueError("La cantidad debe ser mayor a cero")
+    productos.append({
+        "nombre": nombre,
+        "cantidad": cantidad,
+        "precio": precio
+    })
